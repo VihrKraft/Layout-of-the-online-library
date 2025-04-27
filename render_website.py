3bb6447d90397c9a032d90688dbce142a8c07056
VihrKraft <sanefedov09@yandex.ru>
2025-04-27 21:45:43 +0300

    Add paginator

--- render_website.py
+++ render_website.py
@@ -6,7 +6,6 @@ from livereload import Server
 from more_itertools import chunked
 
 
-
 def on_reload():
     with open("meta_data.json", "r", encoding="utf-8") as my_file:
         books_json = my_file.read()
@@ -21,8 +20,11 @@ def on_reload():
     ten_lists_books = list(chunked(books, 10))
     for number, list_books in enumerate(ten_lists_books):
         two_collumns_books = list(chunked(list_books, 2))
+        count_pages = len(ten_lists_books)
         rendered_page = template.render(
             books=two_collumns_books,
+            count_pages=count_pages,
+            this_page=number,
         ) 
         file_path = os.path.join('pages', f'index{number}.html')
         with open(file_path, 'w', encoding="utf8") as file:
